5b8cfe28872d8675092c906080cb103fd78e4b75
ggfinn26 <baristasastra@gmail.com>
2026-09-04 17:51:25 +0700

    pindahin file

--- landing-page/tailwind.config.js
+++ landing-page/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./js/**/*.js"],
+  content: ["./public/*.html", "./public/js/**/*.js"],
   theme: {
     extend: {
       colors: {
